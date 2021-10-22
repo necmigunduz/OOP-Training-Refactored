93ff6acf6d71ac0281b7884348127fe313bca249
Hillary Okello <hillariouskelly@outlook.com>
2021-10-22 11:13:02 +0300

    Create list_rentals_for_person_id method

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -67,4 +67,15 @@ class App
     @rentals << Rental.new(date, @books[book_index], @people[person_index])
     puts "Rental created successfully"
   end
+
+  def list_rentals_for_person_id
+    print "ID of person: "
+    id = gets.chomp.to_i
+
+    rentals = @rentals.filter {|rental| rental.person.id == id}
+    puts "Rentals:"
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+    end
+  end
 end

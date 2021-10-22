bd70c21b1f09ccfae742885dfb545f6a3d7ab481
necmigunduz <necmigunduz@gmail.com>
2021-10-22 18:28:51 +0300

    Fix linters

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -37,25 +37,24 @@ class App
   end
 
   def show_menu
-      puts 'Please choose an option by enterin a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      response = gets.chomp.to_i
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    response = gets.chomp.to_i
   end
 
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/MethodLength
 end
 
-
 def main
   puts "Welcome to School Library App!\n\n"
-  app = App.new 
+  app = App.new
   app.start_app
 end
 

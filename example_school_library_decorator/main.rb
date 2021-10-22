9c66b6f882c358afa84b4480acdee03340de7c60
Hillary Okello <hillariouskelly@outlook.com>
2021-10-22 11:07:38 +0300

    Create method that list all books

---
+++ example_school_library_decorator/main.rb
@@ -0,0 +1,17 @@
+require_relative './student.rb'
+require_relative './teacher.rb'
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def list_books
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+
+end
